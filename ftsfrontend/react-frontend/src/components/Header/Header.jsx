2d7b77131d7331a1bfb52e3123f7b07ffc8176f4
boba <deadryefield@gmail.com>
2025-06-19 07:22:20 +0000

    api linking

--- ftsfrontend/react-frontend/src/components/Header/Header.jsx
+++ ftsfrontend/react-frontend/src/components/Header/Header.jsx
@@ -1,23 +1,31 @@
 import React from 'react'
+import { useAuth } from "../../authentication/authProvider"
 
 const Header = () => {
+    const {userIn} =useAuth()
   return (
     <div className='w-full flex flex-row bg-green-800 justify-between'>
-        <div className="flag ps-4 pb-2">
+        <div className="flag ps-6 pb-2">
             <div className="top text-2xl font-semibold tracking-widest text-gray-50">FTS</div>
             <div className="bottom text-sm text-gray-50">A file system</div>
         </div>
-        <div className="profile flex justify-center items-center pe-4 ">
-            <div className="userInfo pe-3">
-                <p className='text-xs text-gray-50 font-normal'>User Name something</p>
-                <p className='text-xs text-gray-100 font-thin'>something</p>
-            </div>
-            <div className="userbox border-1  bg-green-700 p-2 rounded">
-                <p className='text-xs text-gray-50'>User</p>
-            </div>
+        <div className="profile flex justify-center items-center pe-6 ">
+            {userIn?
+                <>
+                    <div className="userInfo pe-3">
+                        <p className='text-xs text-gray-50 font-normal'>User Name something</p>
+                        <p className='text-xs text-gray-100 font-thin'>something</p>
+                    </div>
+                    <div className="userbox border-1  bg-green-700 p-2 rounded">
+                        <p className='text-xs text-gray-50'>User</p>
+                    </div>
+                
+                </> :<div className="userbox border-1  bg-green-700 p-2 rounded">
+                        <p className='text-xs text-gray-50'>Not logged in</p>
+                    </div>
+            }
+            
         </div>
-
-      
     </div>
   )
 }
--- ftsfrontend/react-frontend/src/components/RightBar/Rightbar.jsx
+++ ftsfrontend/react-frontend/src/components/RightBar/Rightbar.jsx
@@ -1,15 +1,88 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios";
+import { data } from 'react-router-dom';
 
+// https://dev.to/darkmavis1980/fetching-data-with-react-hooks-and-axios-114h
 const Rightbar = () => {
+  const [userData, setData] = useState()
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=>{
+    const fetchData = async ()=>{
+      setLoading(true)
+      try {
+        const response = await axios.get("http://127.0.0.1:8000/accounts/me/",
+           {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        withCredentials: true, // Optional: only needed if cookies are set
+        
+        }
+          )
+          setData(response.data)
+        } catch (error) {
+          console.error("Error fetching user data:", error)
+          setData(error.message)
+        } finally {
+          setLoading(false)
+        }
+      }
+    fetchData()
+  }, [])
+  
+  
+
   return (
     <div className="flex flex-col h-screen bg-green-50 text-green-900 p-4">
       <h2 className="text-sm font-bold mb-6 ps-2">Team Alpha</h2>
       <nav className="space-y-3">
-        <a href="#" className="block ps-2 hover:bg-green-50 rounded text-xs hover:border border-green-100">UserX</a>
+        {loading
+        ? <p>loading</p>
+        :<a href="#" className="block ps-2 hover:bg-green-50 rounded text-xs hover:border border-green-100">{userData['username']}</a>
+        }
+        
         <a href="#" className="block ps-2 hover:bg-green-50 rounded text-xs hover:border border-green-100">User M</a>
         <a href="#" className="block ps-2 hover:bg-green-50 rounded text-xs hover:border border-green-100">User Anus</a>
- 
       </nav>
+
+      <div className='my-4'>
+        <h2 className='font-bold text-sm mb-3 ps-2 mt-6'>User credentials</h2>
+        <nav className="space-y-2">
+          {loading
+          ?<p>loading</p>
+          :<a href="#" className="block ps-2 hover:bg-green-50 rounded text-xs hover:border border-green-100
+            font-thin"
+            >{userData['username']}</a>
+          }
+          {loading
+          ?<p>loading</p>
+          :<a href="#" className="block ps-2 hover:bg-green-50 rounded text-xs hover:border border-green-100
+            font-medium"
+            >Email: {userData['email']}</a>
+          }
+          {loading
+          ?<p>loading</p>
+          :<a href="#" className="block ps-2 hover:bg-green-50 rounded text-xs hover:border border-green-100
+            font-medium"
+            >Team: {userData['belongs_to_team']}</a>
+          }
+          {loading
+          ?<p>loading</p>
+          :<p className="block ps-2 rounded text-xs 
+            font-medium "
+            >Team Acess Level: {userData['team_access_level']}</p>
+          }
+          {loading
+          ?<p>loading</p>
+          :<p className="block ps-2 rounded text-xs 
+            font-medium"
+            >Role in Team: {userData['role']}</p>
+          }
+        </nav>
+        
+
+      </div>
     </div>
   )
 }

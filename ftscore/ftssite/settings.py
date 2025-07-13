00523d348ccecd02b6fd6792a5d331aded46ac0a
boba <deadryefield@gmail.com>
2025-07-13 12:48:48 +0000

    env used

--- ftscore/ftssite/settings.py
+++ ftscore/ftssite/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 from datetime import timedelta
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t1m0si86=%2e9&_*11k=34q+jk58oxw&jq_kfg#99*b(i-t1m!'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
